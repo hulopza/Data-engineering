6eb0944496a97e6a7ab4f315c11c18797c93ee7b
hulopza <76908883+hulopza@users.noreply.github.com>
2023-02-14 17:07:12 -0600

    landing tables created

---
+++ STEDI_Human_Balance/customer_landing.sql
@@ -0,0 +1,22 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` bigint COMMENT 'from deserializer', 
+  `birthday` date COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer',
+  `registrationdate`bigint COMMENT 'from deserializer',
+  `lastupdatedate` bigint COMMENT 'from deserializer',
+  `sharewithpublicasofdate`bigint COMMENT 'from deserializer',
+  `sharewithfriendsasofdate`bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='customername','email','phone','birthday','serialnumber','registrationdate','lastupdatedate','sharewithpublicasofdate','sharewithfriendsasofdate') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://hulopza-warehouse/customer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
---
+++ STEDI_Human_Balance/step_trainer_landing.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+  `sensorreadingtime` bigint COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `distancefromobject` int COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='distanceFromObject,sensorReadingTime,serialNumber') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://hulopza-lakehouse/stepTrainer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
